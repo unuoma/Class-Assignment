152ac697bbb86a21d2650f502d688101bcc6df58
unuoma <umo2@njit.edu>
2019-02-18 18:45:24 -0500

    Changed title
    Signed-off-by: unuoma <umo2@njit.edu>

--- web/index.php
+++ web/index.php
@@ -19,7 +19,7 @@ $app->register(new Silex\Provider\TwigServiceProvider(), array(
 
 $app->get('/', function() use($app) {
   $app['monolog']->addDebug('logging output.');
-  return $app['twig']->render('index.twig', array ('title' => 'Homepage'));
+  return $app['twig']->render('index.twig', array ('title' => 'About'));
 });
 
 $app->run();
